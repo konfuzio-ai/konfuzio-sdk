7f8a7bc04804737aac1a1e968402ebd3ab354dc4
ana <ana@konfuzio.com>
2021-10-22 15:10:40 +0200

    use json load

--- konfuzio_sdk/api.py
+++ konfuzio_sdk/api.py
@@ -159,7 +159,8 @@ def get_document_details(document_id, session=konfuzio_session()):
     """
     url = get_document_api_details_url(document_id, include_extractions=False, extra_fields='bbox,hocr')
     r = retry_get(session, url)
-    data = r.json()
+    data = json.loads(r)
+    # data = r.json()
     text = data["text"]
     annotations = data["annotations"]
     annotations_sets = data["sections"]
