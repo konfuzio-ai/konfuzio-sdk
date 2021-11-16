13170cfff045c1f583caf5b37bd9299701f25cb7
ana <ana@konfuzio.com>
2021-11-16 17:42:24 +0100

    download original pdf version in download_data

--- konfuzio_sdk/cli.py
+++ konfuzio_sdk/cli.py
@@ -161,6 +161,7 @@ def data():
 
     for document in tqdm(training_prj.documents + training_prj.test_documents):
         document.get_file()
+        document.get_file(ocr_version=False)
         document.get_bbox()
         document.get_images()
 
