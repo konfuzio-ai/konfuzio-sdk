037c6779f73c00e6cbb1365bf0c7a86775e37fbf
Christopher Helm <c.helm@tum.de>
2022-01-30 17:27:38 +0100

    fix: settings to None

--- konfuzio_sdk/settings_importer.py
+++ konfuzio_sdk/settings_importer.py
@@ -2,7 +2,6 @@
 
 import os
 import sys
-import time
 
 # This settings file will search for the settings file in the project in which konfuzio_sdk is imported
 sys.path.append(os.getcwd())
@@ -10,14 +9,13 @@ sys.path.append(os.getcwd())
 KONFUZIO_PROJECT_ID = None
 KONFUZIO_USER = None
 KONFUZIO_TOKEN = None
-KONFUZIO_HOST = "https://app.konfuzio.com"
-KONFUZIO_DATA_FOLDER = 'data'
+KONFUZIO_HOST = None
+KONFUZIO_DATA_FOLDER = None
 
 try:
     from settings import *  # NOQA
-
 except ImportError:
-    time.sleep(5)
+    pass  # if there is no settings.py in the working directory
 
 BASE_DIR = os.getcwd()
 DATA_ROOT = os.path.join(BASE_DIR, KONFUZIO_DATA_FOLDER)
