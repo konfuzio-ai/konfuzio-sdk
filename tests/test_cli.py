80a2cecfc7bb4f4c0c876ed5e9cf24b15bc0a284
Christopher Helm <c.helm@tum.de>
2022-02-09 10:00:01 +0100

    test: cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,31 +1,18 @@
 """Validate cli functions."""
-#
-# import logging
-# import os
-# import unittest
-# from unittest.mock import patch
-#
-# import pytest
-#
-# from konfuzio_sdk.cli import init_env, data, init_settings
-#
-# logger = logging.getLogger(__name__)
-# TEST_DOCUMENT = 44823
-# ENV_FILE_EXAMPLE = '''KONFUZIO_HOST = https://app.konfuzio.com
-# KONFUZIO_USER = test@gmail.com
-# KONFUZIO_TOKEN = ""
-# KONFUZIO_DATA_FOLDER = data
-# KONFUZIO_PROJECT_ID = 46'''
-#
-#
-# class TestCLItoDownloadData(unittest.TestCase):
-#     """Test CLI functions."""
-#
-#     def test_data(self):
-#         """
-#         Test the creation of folders for a specific document.
-#
-#         Creates temporarily a settings.py in the tests folder.
-#         """
-#         data()
-#         assert os.path.isdir(os.path.join('data/pdf/44823'))
+
+import subprocess
+import unittest
+
+
+class TestCLI(unittest.TestCase):
+    """Test CLI functions."""
+
+    def test_cli(self):
+        """Test if CLI can be called."""
+        proc = subprocess.Popen(
+            ['konfuzio_sdk', ''], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        process_result, err = proc.communicate()
+
+        # If stderr is not none, use the original image.
+        assert "Please enter a valid command line option." in str(process_result)
