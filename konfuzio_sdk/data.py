ab5e606c051f2ede48c5d06385eb5bcb4da4ae4f
Florian Zyprian <zypriafl@web.de>
2022-02-21 19:08:06 +0100

    Calculate ocr_file_path and original_file_path as property. Compress bounding box files.

--- konfuzio_sdk/data.py
+++ konfuzio_sdk/data.py
@@ -6,6 +6,8 @@ import os
 import pathlib
 import re
 import shutil
+import zipfile
+
 from copy import deepcopy
 import time
 from datetime import tzinfo
@@ -1156,7 +1158,6 @@ class Document(Data):
         # prepare local setup for document
         pathlib.Path(self.document_folder).mkdir(parents=True, exist_ok=True)
         self.image_paths = []  # Path to the images  # todo implement pages
-        self.ocr_file_path = None  # Path to the ocred pdf (sandwich pdf)
         self.annotation_file_path = os.path.join(self.document_folder, "annotations.json5")
         self.annotation_set_file_path = os.path.join(self.document_folder, "annotation_sets.json5")
         self.txt_file_path = os.path.join(self.document_folder, "document.txt")
@@ -1271,6 +1272,24 @@ class Document(Data):
         return self.id_ is not None
 
     @property
+    def ocr_file_path(self):
+        """
+        Return a valid filename to the sandwich PDF (a PDF with text embeddings.
+
+        The filetype is always PDF.
+        """
+        return amend_file_name(self.name, append_text="ocr", new_extension=".pdf")
+
+    @property
+    def original_file_path(self):
+        """
+        Return a valid filename to the original file.
+
+        The filetype can be any supported file.
+        """
+        return amend_file_name(self.name)
+
+    @property
     def annotation_sets(self):
         """Return Annotation Sets of Documents."""
         return self._annotation_sets
@@ -1370,10 +1389,9 @@ class Document(Data):
         :return: Path to the selected file.
         """
         if ocr_version:
-            filename = amend_file_name(self.name, append_text="ocr", new_extension=".pdf")
-            self.ocr_file_path = os.path.join(self.document_folder, filename)
-
-        valid_file_name = amend_file_name(self.name)  # make sure the name online is a valid file name, e.g. rmv ":"
+            valid_file_name = self.ocr_file_path
+        else:
+            valid_file_name = self.original_file_path
         file_path = os.path.join(self.document_folder, valid_file_name)
         if self.status[0] == 2 and (not file_path or not is_file(file_path, raise_exception=False) or update):
             if not is_file(file_path, raise_exception=False) or update:
@@ -1509,6 +1527,9 @@ class Document(Data):
         if is_file(self.bbox_file_path, raise_exception=False):
             with open(self.bbox_file_path, "r", encoding="utf-8") as f:
                 bbox = json.loads(f.read())
+        elif is_file(self.bbox_file_path + '.zip', raise_exception=False):
+            with zipfile.ZipFile(self.bbox_file_path + '.zip', "r") as archive:
+                bbox = json.loads(archive.read('bbox.json5'))
         else:
             logger.warning(f'Start downloading bbox files of all characters {self}.')
             if self.status[0] == 2:
@@ -1516,8 +1537,17 @@ class Document(Data):
                     document_id=self.id_, project_id=self.project.id_, session=self.session, extra_fields="bbox"
                 )
                 bbox = data['bbox']
-                with open(self.bbox_file_path, "w", encoding="utf-8") as f:
-                    json.dump(bbox, f, indent=2, sort_keys=True)
+                # Use the `zipfile` module
+                # `compresslevel` was added in Python 3.7
+                with zipfile.ZipFile(self.bbox_file_path + ".zip", mode="w", compression=zipfile.ZIP_DEFLATED,
+                                     compresslevel=9) as zip_file:
+                    # Dump JSON data
+                    dumped_JSON: str = json.dumps(bbox, indent=2, sort_keys=True)
+                    # Write the JSON data into `data.json` *inside* the ZIP file
+                    zip_file.writestr('bbox.json5', data=dumped_JSON)
+                    # Test integrity of compressed archive
+                    zip_file.testzip()
+
 
         return bbox
 
