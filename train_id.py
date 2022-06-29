a6d653fc80c87a77bded5c15056aa8f572c1af30
Florian Zyprian <zypriafl@web.de>
2022-06-29 13:32:46 +0200

    update awb.

--- train_id.py
+++ train_id.py
@@ -1,6 +1,14 @@
 import logging
+import os
 from typing import List
 
+import pandas
+
+from konfuzio_sdk.api import upload_ai_model
+from konfuzio_sdk.data import Project
+from konfuzio_sdk.evaluate import Evaluation
+from konfuzio_sdk.pipelines.extraction_ai import DocumentAnnotationMultiClassModel
+from konfuzio_sdk.pipelines.load_data import load_pickle
 from konfuzio_sdk.api import upload_ai_model
 from konfuzio_sdk.data import Project
 from konfuzio_sdk.pipelines.base import ExtractionModel
@@ -8,9 +16,19 @@ from konfuzio_sdk.pipelines.extraction_ai import DocumentAnnotationMultiClassMod
 from konfuzio_sdk.pipelines.load_data import load_pickle, get_latest_document_model
 from konfuzio_sdk.tokenizer.base import ListTokenizer
 from konfuzio_sdk.tokenizer.regex import RegexTokenizer
-
 logger = logging.getLogger(__name__)
 
+# if __name__ == "__main__":
+
+#     logger.info("Project initialized.")
+#     category = training_prj.get_category_by_id(14)
+#     # category.label_sets[0].labels = [training_prj.get_label_by_name('PLZ')]
+#     doc_model = DocumentAnnotationMultiClassModel(category=category)
+#     doc_model.configure({'tokenizer_regex_combination': True})
+#     doc_model.build()
+#     doc_model_path = doc_model.save(output_dir=training_prj.model_folder, include_konfuzio=True)
+#     upload_ai_model(doc_model_path, category_ids=[category.id_])
+#
 
 class ListExtractionAI(ExtractionModel):
 
@@ -71,53 +89,46 @@ class LabelModel(DocumentAnnotationMultiClassModel):
         return self
 
 
-if __name__ == "__main__":
-    training_prj = Project(id_=226, update=True)
-    logger.info("Project initialized.")
-    category = training_prj.get_category_by_id(771)
+training_prj = Project(id_=2, update=True)
+category = training_prj.get_category_by_id(id_=17)
 
-    # Merging code.
-    labels = [
-        training_prj.get_label_by_name('Part 2'),
-        training_prj.get_label_by_name('Prefix'),
-        training_prj.get_label_by_name('Issue_Date')
-    ]
-    extraction_ais = []
-    for label in labels:
-        label_model_path = get_latest_document_model(f'*{label.name}.pkl', folder_path=category.project.model_folder)
-        label_model = load_pickle(label_model_path)
-        extraction_ais.append(label_model)
-    extraction_ai = ListExtractionAI(extraction_ais=extraction_ais, category=category)
-    modelpath = extraction_ai.save(include_konfuzio=True, output_dir=category.project.model_folder)
-    upload_ai_model(ai_model_path=modelpath, category_ids=[category.id_])
-    print(modelpath)
+labels = [
+    training_prj.get_label_by_name('Geburtsort'),
+]
 
-    #   labels = [training_prj.get_label_by_name('Prefix')]
-    # labels = [training_prj.get_label_by_name('Part 2')]
-    # labels = [training_prj.get_label_by_name('Issue_Date')]
-    #
-    # extraction_ais = []
-    # for label in labels:
-    #     # Add Regex tokenizers.
-    #     new_tokenizers = []
-    #     regexes = label.find_regex(category=category, annotations=label.annotations(categories=[category]))
-    #     for regex in regexes:
-    #         new_tokenizers.append(RegexTokenizer(regex))
-    #
-    #     # category.project._documents = category.documents()[:1]
-    #     doc_model = LabelModel(category=category, label=label)
-    #     # doc_model.configure(dict(n_nearest_left=10, n_nearest_right=10))
-    #     doc_model.tokenizer = ListTokenizer(tokenizers=new_tokenizers)
-    #     doc_model.build()
-    #     extraction_ais.append(doc_model)
-    #     doc_model.save(output_dir=category.project.model_folder, name=label.name)
+extraction_ais = []
+for label in labels:
+    # Add Regex tokenizers.
+    new_tokenizers = []
+    regexes = label.find_regex(category=category, annotations=label.annotations(categories=[category]))
+    for regex in regexes:
+        new_tokenizers.append(RegexTokenizer(regex))
 
-    # document = category.documents()[0]
-    # extraction_ai = ListExtractionAI(extraction_ais=extraction_ais, category=category)
-    # extraction_result = extraction_ai.extract(text=document.text, bbox=document.get_bbox(), pages=document.pages)
-    # print(extraction_result)
-    #
-    # modelpath = extraction_ai.save(include_konfuzio=True)
-    # doc_model = load_pickle(modelpath)
-    # extraction_result = extraction_ai.extract(text=document.text, bbox=document.get_bbox(), pages=document.pages)
-    # print(extraction_result)
+    # category.project._documents = category.documents()[:1]
+    doc_model = LabelModel(category=category, label=label)
+    # doc_model.configure(dict(n_nearest_left=10, n_nearest_right=10))
+    doc_model.tokenizer = ListTokenizer(tokenizers=new_tokenizers)
+    doc_model.build()
+    extraction_ais.append(doc_model)
+    doc_model.save(output_dir=category.project.model_folder, name=label.name)
+
+
+# # Merging code.
+#
+# extraction_ais = []
+# for label in labels:
+#     label_model_path = get_latest_document_model(f'*{label.name}.pkl', folder_path=category.project.model_folder)
+#     label_model = load_pickle(label_model_path)
+#     extraction_ais.append(label_model)
+# extraction_ai = ListExtractionAI(extraction_ais=extraction_ais, category=category)
+# modelpath = extraction_ai.save(include_konfuzio=True, output_dir=category.project.model_folder)
+# upload_ai_model(ai_model_path=modelpath, category_ids=[category.id_])
+# print(modelpath)
+
+# csv_path = os.path.join(training_prj.model_folder, "2022-06-10-13-13-29.csv")
+# df = pandas.read_csv(csv_path)
+# e = Evaluation(df)
+# print(e)
+# e.label_evaluations()
+# e.label_evaluations(dataset_status=[2])
+# e.label_evaluations(dataset_status=[3])
