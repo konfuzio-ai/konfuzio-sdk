352626dce5c5db139e2fa81c8ba307fa61dc6060
Christopher Helm <c.helm@tum.de>
2022-07-07 00:07:41 +0200

    fix: get version without need to install SDK dependencies
    As found here https://packaging.python.org/en/latest/guides/single-sourcing-package-version/.

--- setup.py
+++ setup.py
@@ -1,12 +1,41 @@
 """Setup."""
+import subprocess
 import sys
 import textwrap
-from os import path
+from datetime import datetime
+from os import path, getenv
 
 import setuptools
 
-# check python version
-from konfuzio_sdk import version
+
+# Define version or calculate it for nightly build.
+#
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y.0   # For first release after an increment in Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.Y.ZaN   # Alpha release
+#   X.Y.ZbN   # Beta release
+#   X.Y.ZrcN  # Release Candidate
+#   X.Y.Z     # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+commit = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
+
+with open(path.join('konfuzio_sdk', 'VERSION')) as version_file:
+    version_number = version_file.read().strip()
+
+if getenv('NIGHTLY_BUILD'):
+    # create a pre-release
+    version = f"{version_number}.dev{datetime.now().strftime('%Y%m%d')}_{commit}"
+else:
+    version = f"{version_number}"
 
 CURRENT_PYTHON = sys.version_info[:2]
 REQUIRED_PYTHON = (3, 6)
@@ -29,11 +58,9 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-VERSION = version.__version__
-
 setuptools.setup(
     name="konfuzio_sdk",
-    version="0.2.3",
+    version=version,
     author='Helm & Nagel GmbH',
     author_email="info@helm-nagel.com",
     description="Konfuzio Software Development Kit",
