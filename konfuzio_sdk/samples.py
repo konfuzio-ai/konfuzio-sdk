9e3101e1d1cbdbbc50730a604dc2a1d5a7acdee3
Christopher Helm <c.helm@tum.de>
2022-07-12 16:10:39 +0200

    add: LocalTextProject overlapping Spans of Annotation

--- konfuzio_sdk/samples.py
+++ konfuzio_sdk/samples.py
@@ -14,6 +14,7 @@ class LocalTextProject(Project):
         category_2 = Category(project=self, id_=2)
         label_set = LabelSet(id_=2, project=self, categories=[category, category_2])
         label = Label(id_=3, text='LabelName', project=self, label_sets=[label_set])
+        label_new = Label(id_=4, text='LabelName 2', project=self, label_sets=[label_set])
 
         self.local_training_document = Document(project=self, category=category, text="Hi all,", dataset_status=2)
         annotation_set = AnnotationSet(id_=4, document=self.local_training_document, label_set=label_set)
@@ -21,10 +22,21 @@ class LocalTextProject(Project):
             id_=5,
             document=self.local_training_document,
             is_correct=True,
+            confidence=1.0,
             annotation_set=annotation_set,
             label=label,
             label_set=label_set,
-            spans=[Span(start_offset=3, end_offset=6)],
+            spans=[Span(start_offset=3, end_offset=5)],
+        )
+        _ = Annotation(
+            id_=6,
+            document=self.local_training_document,
+            is_correct=True,
+            confidence=1.0,
+            annotation_set=annotation_set,
+            label=label_new,
+            label_set=label_set,
+            spans=[Span(start_offset=7, end_offset=10)],
         )
 
         document_test_a = Document(project=self, category=category, text="Hi all,", dataset_status=3)
@@ -36,7 +48,17 @@ class LocalTextProject(Project):
             annotation_set=annotation_set_test_a,
             label=label,
             label_set=label_set,
-            spans=[Span(start_offset=3, end_offset=6)],
+            spans=[Span(start_offset=7, end_offset=10)],
+        )
+
+        _ = Annotation(
+            id_=70,
+            document=document_test_a,
+            is_correct=True,
+            annotation_set=annotation_set_test_a,
+            label=label_new,
+            label_set=label_set,
+            spans=[Span(start_offset=11, end_offset=14)],
         )
 
         document_test_b = Document(project=self, category=category, text="Hi all,", dataset_status=3)
