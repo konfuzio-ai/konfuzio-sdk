78db3c867d8720d51e968bb58c4c713a6da40a10
samuel500 <sknoche@fordham.edu>
2022-11-04 08:39:54 +0000

    typo

--- konfuzio_sdk/__init__.py
+++ konfuzio_sdk/__init__.py
@@ -6,4 +6,4 @@ import logging
 
 logger = logging.getLogger(__name__)
 if '3.7' in sys.version:
-    logger.warning("Some Konfuzio SDK functionalities may not work with Python 3.7. We recomment using Python 3.8.")
+    logger.warning("Some Konfuzio SDK functionalities may not work with Python 3.7. We recommend using Python 3.8.")
