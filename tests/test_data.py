3dee8a4da362147325bbd6d835b86a15a7a3acc3
Davide Zagami <dz@konfuzio.com>
2022-12-16 13:37:25 +0100

    comment to explain why a bbox can be incomplete

--- tests/test_data.py
+++ tests/test_data.py
@@ -537,11 +537,14 @@ class TestOfflineDataSetup(unittest.TestCase):
         assert annotation.end_offset == ann_bbox['end_offset']
 
     def test_add_annotation_with_incomplete_bbox_data(self):
-        """Test to add an Annotation via incomplete bboxes param."""
+        """Test to add an Annotation via bboxes param that is missing offset information."""
         document = Document(project=self.project, category=self.category, text='hello', strict_bbox_validation=True)
         page = Page(id_=None, document=document, start_offset=0, end_offset=4, number=1, original_size=(12, 6))
         document_bbox = {'1': Bbox(x0=0, x1=1, y0=0, y1=1, page=page)}
         document.set_bboxes(document_bbox)
+        # An Annotation can be created by providing a list of Spans or a list of bboxes.
+        # In the latter case, the minimum information required is the start and end offsets corresponding
+        # to the characters of each bbox.
         annotation_bboxes = [{'start_offset': 0, 'end_offset': 1}, {'start_offset': 3}]
         with pytest.raises(ValueError, match='cannot read bbox'):
             Annotation(document=document, bboxes=annotation_bboxes, label=self.label, label_set=self.label_set)
