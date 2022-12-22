1efb5ea7f9e25b9a78e5d752283340aed9d9f317
da-konfuzio <102763000+da-konfuzio@users.noreply.github.com>
2022-12-22 13:04:13 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ language = 'en'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# make sure that make html starts with the tutorials.rst
+# make sure that make html starts with the index.rst
 master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
