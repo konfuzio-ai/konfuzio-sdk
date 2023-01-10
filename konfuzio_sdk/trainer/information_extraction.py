c8042bf1544eb4767b7641ccf5d80fe96cd7b39c
Elizaveta <k4www4@gmail.com>
2023-01-10 18:21:45 +0400

    removed usage of Category is None

--- konfuzio_sdk/trainer/information_extraction.py
+++ konfuzio_sdk/trainer/information_extraction.py
@@ -1767,7 +1767,7 @@ class GroupAnnotationSets:
         :param df:
         :return:
         """
-        if self.category is None:
+        if self.category.name == 'NO_CATEGORY':
             raise AttributeError(f'{self} does not provide a Category.')
 
         global_df = pandas.DataFrame()
