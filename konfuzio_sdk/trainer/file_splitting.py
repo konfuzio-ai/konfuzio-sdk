63c45f5eb80ab9a605dbadbfa16c9a4a332bba7a
Elizaveta <k4www4@gmail.com>
2023-01-12 12:57:07 +0400

    paths fix

--- konfuzio_sdk/trainer/file_splitting.py
+++ konfuzio_sdk/trainer/file_splitting.py
@@ -44,7 +44,7 @@ class AbstractFileSplittingModel(BaseModel, metaclass=abc.ABCMeta):
         """Generate a path for temporary pickle file."""
         temp_pkl_file_path = os.path.join(
             self.output_dir,
-            f'{get_timestamp(konfuzio_format="%Y-%m-%d-%H-%M")}_{self.name_lower()}_{self.project.id_}_' f'tmp.pkl',
+            f'{get_timestamp(konfuzio_format="%Y-%m-%d-%H")}_{self.name_lower()}_{self.project.id_}_' f'tmp.pkl',
         )
         return temp_pkl_file_path
 
@@ -53,7 +53,7 @@ class AbstractFileSplittingModel(BaseModel, metaclass=abc.ABCMeta):
         """Generate a path for a resulting pickle file."""
         pkl_file_path = os.path.join(
             self.output_dir,
-            f'{get_timestamp(konfuzio_format="%Y-%m-%d-%H-%M")}_{self.name_lower()}_{self.project.id_}' f'.pkl',
+            f'{get_timestamp(konfuzio_format="%Y-%m-%d-%H")}_{self.name_lower()}_{self.project.id_}' f'.pkl',
         )
         return pkl_file_path
 
