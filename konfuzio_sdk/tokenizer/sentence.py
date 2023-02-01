f5fdf244922a56fc4b25f160a1eb8c654dce9aaf
samuel500 <sknoche@fordham.edu>
2023-02-01 17:12:39 +0000

    sentence tokenizer detectron

--- konfuzio_sdk/tokenizer/sentence.py
+++ konfuzio_sdk/tokenizer/sentence.py
@@ -22,3 +22,138 @@ class SentenceTokenizer(AbstractTokenizer):
         :param mode: line_distance or detectron
         """
         self.mode = mode
+
+    def __repr__(self):
+        """Return string representation of the class."""
+        return f"{self.__class__.__name__}: {self.mode=}"
+
+    def __hash__(self):
+        """Get unique hash for RegexTokenizer."""
+        return hash(repr(self))
+
+    def __eq__(self, other) -> bool:
+        """Compare Tokenizer with another Tokenizer."""
+        return hash(self) == hash(other)
+
+    def tokenize(self, document: Document) -> Document:
+        """Create one multiline Annotation per paragraph detected."""
+        assert sdk_isinstance(document, Document)
+
+        before_none = len(document.annotations(use_correct=False, label=document.project.no_label))
+
+        t0 = time.monotonic()
+
+        if not document.bboxes_available:
+            raise ValueError(
+                f"Cannot tokenize Document {document} with tokenizer {self}: Missing Character Bbox information."
+            )
+
+        if self.mode == 'detectron':
+            self._detectron_tokenize(document=document)
+        elif self.mode == 'line_distance':
+            self._line_distance_tokenize(document=document)
+
+        after_none = len(document.annotations(use_correct=False, label=document.project.no_label))
+        logger.info(f'{after_none - before_none} new Annotations in {document} by {repr(self)}.')
+
+        self.processing_steps.append(ProcessingStep(self, document, time.monotonic() - t0))
+
+        return document
+
+    def _detectron_tokenize(self, document: Document) -> Document:
+        """Create one multiline Annotation per sentence detected in paragraph detected by detectron2."""
+        assert isinstance(document.project.id_, int)
+        doc_id = document.id_ if document.id_ else document.copy_of_id
+        paragraph_bboxes = get_results_from_segmentation(doc_id, document.project.id_)
+
+        assert len(paragraph_bboxes) == document.number_of_pages
+
+        # normalize
+        for i, page in enumerate(document.pages()):
+            scale_mult = page.height / page.full_height
+            for bbox in paragraph_bboxes[i]:
+                bbox['x0'] *= scale_mult
+                bbox['x1'] *= scale_mult
+                bbox['y0'] *= scale_mult
+                bbox['y1'] *= scale_mult
+
+        # assemble characters by their page
+        pages_char_bboxes = [[] for _ in document.pages()]
+        for char_index, bbox in document.get_bbox().items():
+            bbox['char_index'] = int(char_index)
+            pages_char_bboxes[bbox['page_number'] - 1].append(bbox)
+
+        assert len(pages_char_bboxes) == len(paragraph_bboxes)
+
+        # assemble character by their pargraph
+        paragraph_char_bboxes = collections.defaultdict(list)
+        for i, (page_char_booxes, page_paragraph_bboxes) in enumerate(zip(pages_char_bboxes, paragraph_bboxes)):
+            page = document.get_page_by_index(i)
+            for bbox in sorted(page_char_booxes, key=lambda x: x['char_index']):
+                for paragraph_bbox in page_paragraph_bboxes:
+                    if (
+                        bbox['x0'] <= paragraph_bbox['x1']
+                        and bbox['x1'] >= paragraph_bbox['x0']
+                        and page.height - bbox['y0'] <= paragraph_bbox['y1']
+                        and page.height - bbox['y1'] >= paragraph_bbox['y0']
+                    ):
+                        paragraph_char_bboxes[
+                            (
+                                int(paragraph_bbox['x0']),
+                                int(paragraph_bbox['x1']),
+                                int(paragraph_bbox['y0']),
+                                int(paragraph_bbox['y1']),
+                            )
+                        ].append(bbox)
+                        break
+
+        for k, bboxes in paragraph_char_bboxes.items():
+            spans = []
+            line_bboxes = []
+            for bbox in bboxes:
+                if not line_bboxes or bbox['line_number'] == line_bboxes[0]['line_number']:
+                    line_bboxes.append(bbox)
+                else:
+                    spans.append(
+                        Span(start_offset=line_bboxes[0]['char_index'], end_offset=line_bboxes[-1]['char_index'] + 1)
+                    )
+                    line_bboxes = [bbox]
+
+                if bbox['text'] in {'.', '!', '?'}:
+                    spans.append(
+                        Span(start_offset=line_bboxes[0]['char_index'], end_offset=line_bboxes[-1]['char_index'] + 1)
+                    )
+                    line_bboxes = []
+                    _ = Annotation(
+                        document=document,
+                        annotation_set=document.no_label_annotation_set,
+                        label=document.project.no_label,
+                        label_set=document.project.no_label_set,
+                        category=document.category,
+                        spans=spans,
+                    )
+                    spans = []
+
+            if line_bboxes:
+                spans.append(
+                    Span(start_offset=line_bboxes[0]['char_index'], end_offset=line_bboxes[-1]['char_index'] + 1)
+                )
+            if spans:
+                _ = Annotation(
+                    document=document,
+                    annotation_set=document.no_label_annotation_set,
+                    label=document.project.no_label,
+                    label_set=document.project.no_label_set,
+                    category=document.category,
+                    spans=spans,
+                )
+
+        return document
+
+    def _line_distance_tokenize(self, document: Document) -> Document:
+        """Create one multiline Annotation per sentence in paragraph detected by line distance based rule based algo."""
+        pass
+
+    def found_spans(self, document: Document):
+        """Sentence found spans."""
+        pass
