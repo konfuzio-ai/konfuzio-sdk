0604c17f673620a57a110c6a233f6e68b43fd3f3
samuel500 <sknoche@fordham.edu>
2023-02-21 17:09:33 +0000

    overlapping spans horizontal merge fix

--- konfuzio_sdk/data.py
+++ konfuzio_sdk/data.py
@@ -1336,8 +1336,11 @@ class Span(Data):
         )
 
     def __lt__(self, other: 'Span'):
-        """If we sort spans we do so by start offset."""
-        return self.start_offset < other.start_offset
+        """We compare Spans by their start offset. If start offsets are equal, we use end offsets."""
+        if self.start_offset == other.start_offset:
+            return self.end_offset < other.end_offset
+        else:
+            return self.start_offset < other.start_offset
 
     def __repr__(self):
         """Return string representation."""
