0b55d23c04664e8869105ec01f09fe85ce72ac18
samuel500 <sknoche@fordham.edu>
2023-02-28 20:58:55 +0000

    tfcpu no version

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setuptools.setup(
         'requests',  # todo add ==2.24.0 ?
         'regex>=2020.6.8',  # re module but better
         'tabulate>=0.9.0',  # Used to pretty print DataFrames
-        'tensorflow-cpu>=2.11.0',
+        'tensorflow-cpu',
         'torch>=1.12.1',
         'transformers>=4.21.2',
         'tqdm',
