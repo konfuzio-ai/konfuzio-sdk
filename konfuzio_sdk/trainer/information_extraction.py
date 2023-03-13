aa2c3cb183435fbed573dfe2c0ef391ec6a54969
samuel500 <sknoche@fordham.edu>
2023-03-13 09:05:50 +0000

    Fixed: A minor bug that caused extracted Documents to have two different default AnnotationSets (#249)

--- konfuzio_sdk/trainer/information_extraction.py
+++ konfuzio_sdk/trainer/information_extraction.py
@@ -1036,38 +1036,6 @@ def generate_feature_dict_from_occurence_dict(occurence_dict, catchphrase_list, 
     return _dict
 
 
-def add_extractions_as_annotations(
-    extractions: pandas.DataFrame, document: Document, label: Label, label_set: LabelSet, annotation_set: AnnotationSet
-) -> None:
-    """Add the extraction of a model to the document."""
-    if not isinstance(extractions, pandas.DataFrame):
-        raise TypeError(f'Provided extraction object should be a Dataframe, got a {type(extractions)} instead')
-    if not extractions.empty:
-        # TODO: define required fields
-        required_fields = ['start_offset', 'end_offset', 'confidence']
-        if not set(required_fields).issubset(extractions.columns):
-            raise ValueError(
-                f'Extraction do not contain all required fields: {required_fields}.'
-                f' Extraction columns: {extractions.columns.to_list()}'
-            )
-
-        extracted_spans = extractions[required_fields].sort_values(by='confidence', ascending=False)
-
-        for span in extracted_spans.to_dict('records'):  # todo: are start_offset and end_offset always ints?
-            annotation = Annotation(
-                document=document,
-                label=label,
-                confidence=span['confidence'],
-                label_set=label_set,
-                annotation_set=annotation_set,
-                spans=[Span(start_offset=span['start_offset'], end_offset=span['end_offset'])],
-            )
-            if annotation.spans[0].offset_string is None:
-                raise NotImplementedError(
-                    f"Extracted {annotation} does not have a correspondence in the " f"text of {document}."
-                )
-
-
 class BaseModel(metaclass=abc.ABCMeta):
     """Base model to define common methods for all AIs."""
 
@@ -1272,42 +1240,85 @@ class Trainer(BaseModel):
         virtual_default_annotation_set = AnnotationSet(
             document=virtual_doc, label_set=category_label_set, id_=virtual_annotation_set_id
         )
-
+        virtual_annotation_set_id += 1
         for label_or_label_set_name, information in extraction_result.items():
-            if isinstance(information, pandas.DataFrame) and not information.empty:
+
+            if isinstance(information, pandas.DataFrame):
+                if information.empty:
+                    continue
+
                 # annotations belong to the default Annotation Set
                 label = self.category.project.get_label_by_name(label_or_label_set_name)
-                add_extractions_as_annotations(
+                self.add_extractions_as_annotations(
                     document=virtual_doc,
                     extractions=information,
                     label=label,
                     label_set=category_label_set,
                     annotation_set=virtual_default_annotation_set,
                 )
-
-            elif isinstance(information, list) or isinstance(information, dict):
-                # process multi Annotation Sets that are not part of the category Label Set
+            # process multi Annotation Sets that are not part of the category Label Set
+            else:
                 label_set = self.category.project.get_label_set_by_name(label_or_label_set_name)
 
                 if not isinstance(information, list):
                     information = [information]
 
                 for entry in information:  # represents one of pot. multiple annotation-sets belonging of one LabelSet
-                    virtual_annotation_set_id += 1
-                    virtual_annotation_set = AnnotationSet(
-                        document=virtual_doc, label_set=label_set, id_=virtual_annotation_set_id
-                    )
+                    if label_set is not category_label_set:
+                        virtual_annotation_set = AnnotationSet(
+                            document=virtual_doc, label_set=label_set, id_=virtual_annotation_set_id
+                        )
+                        virtual_annotation_set_id += 1
+                    else:
+                        virtual_annotation_set = virtual_default_annotation_set
 
                     for label_name, extractions in entry.items():
                         label = self.category.project.get_label_by_name(label_name)
-                        add_extractions_as_annotations(
+                        self.add_extractions_as_annotations(
                             document=virtual_doc,
                             extractions=extractions,
                             label=label,
                             label_set=label_set,
                             annotation_set=virtual_annotation_set,
                         )
+
         return virtual_doc
+
+    @staticmethod
+    def add_extractions_as_annotations(
+        extractions: pandas.DataFrame,
+        document: Document,
+        label: Label,
+        label_set: LabelSet,
+        annotation_set: AnnotationSet,
+    ) -> None:
+        """Add the extraction of a model to the document."""
+        if not isinstance(extractions, pandas.DataFrame):
+            raise TypeError(f'Provided extraction object should be a Dataframe, got a {type(extractions)} instead')
+        if not extractions.empty:
+            # TODO: define required fields
+            required_fields = ['start_offset', 'end_offset', 'confidence']
+            if not set(required_fields).issubset(extractions.columns):
+                raise ValueError(
+                    f'Extraction do not contain all required fields: {required_fields}.'
+                    f' Extraction columns: {extractions.columns.to_list()}'
+                )
+
+            extracted_spans = extractions[required_fields].sort_values(by='confidence', ascending=False)
+
+            for span in extracted_spans.to_dict('records'):  # todo: are start_offset and end_offset always ints?
+                annotation = Annotation(
+                    document=document,
+                    label=label,
+                    confidence=span['confidence'],
+                    label_set=label_set,
+                    annotation_set=annotation_set,
+                    spans=[Span(start_offset=span['start_offset'], end_offset=span['end_offset'])],
+                )
+                if annotation.spans[0].offset_string is None:
+                    raise NotImplementedError(
+                        f"Extracted {annotation} does not have a correspondence in the " f"text of {document}."
+                    )
 
     @classmethod
     def merge_horizontal(cls, res_dict: Dict, doc_text: str) -> Dict:
