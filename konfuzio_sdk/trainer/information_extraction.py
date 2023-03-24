be8e537e973b04630033b169d2153ada77f70dab
samuel500 <sknoche@fordham.edu>
2023-03-24 13:36:41 +0000

    extraction category change

--- konfuzio_sdk/trainer/information_extraction.py
+++ konfuzio_sdk/trainer/information_extraction.py
@@ -1904,6 +1904,7 @@ class RFExtractionAI(Trainer, GroupAnnotationSets):
         inference_document = deepcopy(document)
 
         # In case document category was changed after RFExtractionAI training
+        inference_document._category = None
         inference_document.set_category(self.category)
 
         # 2. tokenize
