380c3c087476072482729a5efc250d4ce4fd446e
Elizaveta <k4www4@gmail.com>
2023-03-29 19:38:25 +0400

    documentation fix

--- docs/sdk/boilerplates/test_outlier_annotations.py
+++ docs/sdk/boilerplates/test_outlier_annotations.py
@@ -1,5 +1,9 @@
 """Test searching for the outlier Annotations under a given Label."""
 from konfuzio_sdk.data import Project
+from konfuzio_sdk.evaluate import ExtractionEvaluation
+from konfuzio_sdk.tokenizer.base import ListTokenizer
+from konfuzio_sdk.tokenizer.regex import RegexTokenizer
+from konfuzio_sdk.trainer.information_extraction import RFExtractionAI
 from variables import YOUR_PROJECT_ID
 
 YOUR_LABEL_NAME = 'Austellungsdatum'
@@ -7,17 +11,40 @@ YOUR_LABEL_NAME = 'Austellungsdatum'
 project = Project(id_=YOUR_PROJECT_ID)
 label = project.get_label_by_name('Bank inkl. IBAN')
 outliers = label.get_probable_outliers_by_regex(project.categories)
+outliers = label.get_probable_outliers_by_regex(project.categories, top_worst_percentage=1.0)
 outlier_spans = [span.offset_string for annotation in outliers for span in annotation.spans]
 assert len(outliers) == 16
 assert 'Deutsche Bank PGK  Nürnbe' in outlier_spans
 assert 'DE73 7607 0024 0568 9745 11' in outlier_spans
-label = project.get_label_by_name('Firmenname')
-outliers = label.get_probable_outliers_by_confidence(project.categories, n_outliers=3)
+label = project.get_label_by_name(YOUR_LABEL_NAME)
+pipeline = RFExtractionAI()
+pipeline.tokenizer = ListTokenizer(tokenizers=[])
+pipeline.category = label.project.get_category_by_id(id_=63)
+train_doc_ids = {44823, 44834, 44839, 44840, 44841}
+pipeline.documents = [doc for doc in pipeline.category.documents() if doc.id_ in train_doc_ids]
+for cur_label in pipeline.category.labels:
+    for regex in cur_label.find_regex(category=pipeline.category):
+        pipeline.tokenizer.tokenizers.append(RegexTokenizer(regex=regex))
+pipeline.test_documents = pipeline.category.test_documents()
+pipeline.df_train, pipeline.label_feature_list = pipeline.feature_function(
+    documents=pipeline.documents, require_revised_annotations=False
+)
+pipeline.fit()
+predictions = []
+for doc in pipeline.documents:
+    predicted_doc = pipeline.extract(document=doc)
+    predictions.append(predicted_doc)
+evaluation = ExtractionEvaluation(documents=list(zip(pipeline.documents, predictions)), strict=False)
+outliers = label.get_probable_outliers_by_confidence(evaluation, 0.8)
+assert len(outliers) == 1
+outliers = label.get_probable_outliers_by_confidence(evaluation)
+outlier_spans = [span.offset_string for annotation in outliers for span in annotation.spans]
+assert '24.05.2018' in outlier_spans
+outliers = label.get_probable_outliers_by_confidence()
 assert len(outliers) == 3
 for annotation in outliers:
     assert annotation.confidence < 0.5
     assert annotation.is_correct
-label = project.get_label_by_name(YOUR_LABEL_NAME)
 outliers = label.get_probable_outliers_by_normalization(project.categories)
 outlier_spans = [span.offset_string for annotation in outliers for span in annotation.spans]
 assert len(outliers) == 1
--- docs/sdk/examples/outlier-annotations/index.md
+++ docs/sdk/examples/outlier-annotations/index.md
@@ -1,31 +1,30 @@
 ## Find possible outliers among the ground-truth Annotations
 
 If you want to ensure that Annotations of a Label are consistent and check for possible outliers, you can use one of 
-the `Label` class's methods. There are three of them available; all of them require a list of Categories passed at the 
-input to iterate over them upon fetching the Annotations.
+the `Label` class's methods. There are three of them available.
 
 - `get_probable_outliers_by_regex` looks for the worst regexes used to find the Annotations. "Worst" is determined by
 the number of True Positives calculated upon evaluating the regexes' performance. Returns Annotations predicted by the
-regexes with the least amount of True Positives. The default number of top worst regexes to return the Annotation from 
-is 3.
+regexes with the least amount of True Positives. By default, the method returns Annotations retrieved by the regex that
+performs on the level of 10% in comparison to the best one.
 
   .. literalinclude:: /sdk/boilerplates/test_outlier_annotations.py
      :language: python
-     :lines: 2,6-7,20,9
+     :lines: 2,10-11,19,13
 
 - `get_probable_outliers_by_confidence` looks for the Annotations with the least confidence level, provided it is lower
-than 0.5. 
+than 0.5. Accepts an instance of EvaluationExtraction class as an input and uses confidence predictions from there.
    
   .. literalinclude:: /sdk/boilerplates/test_outlier_annotations.py
      :language: python
-     :lines: 2,6-7,20,15
+     :lines: 2,10-11,19,40
 
 - `get_probable_outliers_by_normalization` looks for the Annotations that are unable to pass normalization by the data
 type of the given Label (meaning that they are not of the same data type themselves, thus outliers).
 
   .. literalinclude:: /sdk/boilerplates/test_outlier_annotations.py
      :language: python
-     :lines: 2,6-7,20,21
+     :lines: 2,10-11,19,48
 
 All three of the methods return a list of Annotations that are deemed outliers by the logic of the current method; the 
 contents of the output are not necessarily wrong, however, they may have some difference from the main body of the 
@@ -39,4 +38,4 @@ three of the search methods are enabled.
 
 .. literalinclude:: /sdk/boilerplates/test_outlier_annotations.py
    :language: python
-   :lines: 2,6-7,20,26
+   :lines: 2,10-11,19,53
