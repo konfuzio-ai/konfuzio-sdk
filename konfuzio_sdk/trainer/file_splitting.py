27078368e59d8deeb647d579c0012193b4fbd367
Elizaveta <k4www4@gmail.com>
2023-03-30 18:28:04 +0400

    updated the naming and check_is_ready among the AIs

--- konfuzio_sdk/trainer/file_splitting.py
+++ konfuzio_sdk/trainer/file_splitting.py
@@ -85,7 +85,7 @@ class AbstractFileSplittingModel(BaseModel, metaclass=abc.ABCMeta):
         """
         temp_pkl_file_path = os.path.join(
             self.output_dir,
-            f'{get_timestamp()}_{self.name_lower()}_tmp.pkl',
+            f'{get_timestamp()}_{self.project.id_}_{self.name_lower()}_tmp.pkl',
         )
         return temp_pkl_file_path
 
@@ -98,7 +98,7 @@ class AbstractFileSplittingModel(BaseModel, metaclass=abc.ABCMeta):
         """
         pkl_file_path = os.path.join(
             self.output_dir,
-            f'{get_timestamp()}_{self.name_lower()}.pkl',
+            f'{get_timestamp()}_{self.project.id_}_{self.name_lower()}.pkl',
         )
         return pkl_file_path
 
