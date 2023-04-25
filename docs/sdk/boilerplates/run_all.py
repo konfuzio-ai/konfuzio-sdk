804628ea15c1534acbbdca9c483288bd1e2df8c6
Elizaveta <k4www4@gmail.com>
2023-04-25 18:30:28 +0200

    fixed rest api text

--- docs/sdk/boilerplates/run_all.py
+++
@@ -1,8 +0,0 @@
-"""Run consecutively all tests."""
-import os
-import glob
-
-for file in glob.iglob("docs/sdk/boilerplates/*.py"):
-    if file != 'docs/sdk/boilerplates/run_all.py':
-        if file == 'docs/sdk/boilerplates/test_train_label_regex_tokenizer.py':
-            os.system("python " + file)
