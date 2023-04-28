4917abf9f6cee851aede3c88a302cf577da605b3
samuel500 <sknoche@fordham.edu>
2023-04-28 17:46:09 +0100

    segmentation boilerplate test

--- docs/sdk/boilerplates/test_segmentation_and_bboxes.py
+++ docs/sdk/boilerplates/test_segmentation_and_bboxes.py
@@ -2,11 +2,11 @@
 from PIL import Image, ImageDraw
 from konfuzio_sdk.data import Project
 from konfuzio_sdk.api import get_results_from_segmentation
-from variables import YOUR_PROJECT_ID
+from variables import YOUR_PROJECT_ID, TEST_DOCUMENT_ID
 
 my_project = Project(id_=YOUR_PROJECT_ID, strict_data_validation=False)
 # first Document uploaded
-document = my_project.documents[1]
+document = my_project.get_document_by_id(TEST_DOCUMENT_ID)
 # index of the Page to test
 page_index = 0
 
@@ -28,16 +28,16 @@ image = image.resize((int(image.size[0] * factor_x), int(image.size[1] * factor_
 height = image.size[1]
 
 image_characters_bbox = [char_bbox for _, char_bbox in page.get_bbox().items()]
-assert len(image_characters_bbox) == 2248
+assert len(image_characters_bbox) == 2249
 
 draw = ImageDraw.Draw(image)
 
 for bbox in image_characters_bbox:
     image_bbox = (
         int(bbox["x0"]),
-        int((height - bbox["y0"])),
-        int(bbox["x1"]),
         int((height - bbox["y1"])),
+        int(bbox["x1"]),
+        int((height - bbox["y0"])),
     )
     draw.rectangle(image_bbox, outline='blue', width=1)
 
@@ -56,7 +56,7 @@ for bbox in image_segmentation_bboxes[page_index]:
     image_bbox = (
         int(bbox["x0"] * factor_x),
         int(bbox["y0"] * factor_y),
-        int(bbox["x1"] * factor_y),
+        int(bbox["x1"] * factor_x),
         int(bbox["y1"] * factor_y),
     )
     draw.rectangle(image_bbox, outline='blue', width=1)
