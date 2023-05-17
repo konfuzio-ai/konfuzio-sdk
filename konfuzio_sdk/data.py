8f8ea654747d2e6734b31bb685bb36c51a07c74e
Edwin Genego <info@genego.io>
2023-05-18 06:41:25 +0700

    Add detailed error messages to bbox validation

--- konfuzio_sdk/data.py
+++ konfuzio_sdk/data.py
@@ -532,7 +532,7 @@ class Bbox:
 
         if round(self.y1, round_decimals) > round(self.page.height, round_decimals):
             exception_or_log_error(
-                msg=f"{self} exceeds height of {self.page}.",
+                msg=f"{self} exceeds height of {self.page} by {round(self.y1, round_decimals) - round(self.page.height, round_decimals)}.",
                 fail_loudly=validation is not BboxValidationTypes.DISABLED,
                 exception_type=ValueError,
                 handler=handler,
@@ -540,7 +540,7 @@ class Bbox:
 
         if round(self.x1, round_decimals) > round(self.page.width, round_decimals):
             exception_or_log_error(
-                msg=f"{self} exceeds width of {self.page}.",
+                msg=f"{self} exceeds width of {self.page} by {round(self.x1, round_decimals) - round(self.page.width, round_decimals)}.",
                 fail_loudly=validation is not BboxValidationTypes.DISABLED,
                 exception_type=ValueError,
                 handler=handler,
