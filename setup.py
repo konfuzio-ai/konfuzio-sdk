84e57bad85a132fc9ebbde2fe8c3e09fc3febeb6
Liza Ezhergina <k4www4@gmail.com>
2023-05-25 16:59:04 +0400

    initial commit

--- setup.py
+++ setup.py
@@ -91,6 +91,7 @@ setuptools.setup(
         'transformers>=4.21.2',  # huggingface transformers and tokenizers
         'tqdm',
         'pympler>=1.0.1',  # Use to get pickle file size.
+        'pydantic==1.10.8',
         'scikit-learn>=1.0.2',
         'timm==0.6.7',  # for extra pytorch models, i.e. EfficientNet
         'spacy>=2.3.5, <=3.1.4',  # used for spaCy tokenization
