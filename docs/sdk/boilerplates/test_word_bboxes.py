36fcf9c9139baf3f9ef3168c99ee20a1d4fe4602
iftwigs <42752431+iftwigs@users.noreply.github.com>
2023-06-09 18:24:02 +0400

    Changed: Code examples in the Documentation contain relative references to the .py scripts (#309)
    * initial commit
    
    * updated tests
    
    * more updated tests
    
    * more updated tests
    
    * moved variables
    
    * moved variables
    
    * revert
    
    * subheader link
    
    * fix in path

--- docs/sdk/boilerplates/test_word_bboxes.py
+++ docs/sdk/boilerplates/test_word_bboxes.py
@@ -3,20 +3,34 @@
 
 def test_word_bboxes():
     """Test creation of the word-level Bboxes."""
+    # start import
     from copy import deepcopy
     from konfuzio_sdk.data import Project
     from konfuzio_sdk.tokenizer.regex import WhitespaceTokenizer
 
+    # end import
     from tests.variables import TEST_PROJECT_ID, TEST_DOCUMENT_ID
 
     YOUR_PROJECT_ID, YOUR_DOCUMENT_ID = TEST_PROJECT_ID, TEST_DOCUMENT_ID
+    # start project
     project = Project(id_=YOUR_PROJECT_ID)
+    # end project
     project = Project(id_=YOUR_PROJECT_ID, strict_data_validation=False)
+    # start document
     document = project.get_document_by_id(YOUR_DOCUMENT_ID)
+    # end document
+    # start copy
     document = deepcopy(document)
+    # end copy
+    # start tokenize
     tokenizer = WhitespaceTokenizer()
     document = tokenizer.tokenize(document)
+    # end tokenize
     assert len(document.spans()) == 309
+    # start image
     document.get_page_by_index(0).get_annotations_image(display_all=True)
+    # end image
+    # start spans
     span_bboxes = [span.bbox() for span in document.spans()]
+    # end spans
     assert len(span_bboxes) == 309
