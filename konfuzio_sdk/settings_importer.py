0c65da3e3fcbea8a054ab016bd963bb6cb7dede1
Liza Ezhergina <k4www4@gmail.com>
2023-06-20 15:21:11 +0400

    extras list

--- konfuzio_sdk/settings_importer.py
+++ konfuzio_sdk/settings_importer.py
@@ -37,8 +37,7 @@ OPTIONAL_IMPORT_ERROR = "A library *modulename* has not been found."
 extras = [
     'chardet==5.1.0',
     'cloudpickle==2.2.1',
-    'pydantic==1.10.8',  # pydantic is used by spacy. We need to force a higher pydantic version to avoid
-    # https://github.com/tiangolo/fastapi/issues/5048
+    'pydantic==1.10.8',
     'torch>=1.8',
     'torchvision>=0.9',
     'transformers>=4.21.2',
