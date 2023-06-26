b6c599ba1d4aa41a71ebad6b15f567f8ef7bb727
Liza Ezhergina <k4www4@gmail.com>
2023-06-26 16:34:49 +0400

    merge conflicts resolved

--- extras_list.py
+++ extras_list.py
@@ -9,6 +9,7 @@ EXTRAS = {
         'pre-commit',
         'parameterized',
         'Sphinx==4.4.0',
+        'sphinx-toolbox==3.4.0',
         'sphinx-reload==0.2.0',
         'sphinx-notfound-page==0.8',
         'm2r2==0.3.2',
