c619dc85c5fbe70792d6f0f36cec0f27def2192d
Tiziano Cocciò <tiziano@helm-nagel.com>
2023-10-24 18:00:22 +0200

    convert md to ipynb during workflow

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,8 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'sdk/tutorials/*.ipynb']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
+                    'sdk/tutorials/*.ipynb', 'sdk/tutorials/**/*.md']
 
 # make sure that make html starts with the index.rst
 master_doc = 'index'
