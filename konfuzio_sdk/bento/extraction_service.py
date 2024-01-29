0d719b3a9d0caeab438f57bee87829ebe1cf12c2
Elizaveta Ezhergina <k4www4@gmail.com>
2024-01-29 18:39:20 +0400

    Changed: structure of extraction service's response

--- konfuzio_sdk/bento/extraction_service.py
+++ konfuzio_sdk/bento/extraction_service.py
@@ -1,7 +1,9 @@
+"""Run extraction service for a dockerized AI."""
+
 import bentoml
 from pydantic import BaseModel
 from typing import Optional, List, Tuple
-from konfuzio_sdk.data import Document, Project, Category, Page
+from konfuzio_sdk.data import Document, Project, Page
 
 extraction_runner = bentoml.picklable_model.get("extraction:latest").to_runner()
 
@@ -9,10 +11,14 @@ svc = bentoml.Service("extraction_svc", runners=[extraction_runner])
 
 
 class ExtractRequest20240117(BaseModel):
+    """Describe a scheme for the extraction request on 17/01/2024."""
+
     text: str
     bboxes: Optional[dict]
 
     class Page(BaseModel):
+        """Describe a scheme for the Page class on 17/01/2024."""
+
         number: int
         image: Optional[bytes]
         original_size: Tuple[float, float]
@@ -21,7 +27,11 @@ class ExtractRequest20240117(BaseModel):
 
 
 class ExtractResponse20240117(BaseModel):
+    """Describe a scheme for the extraction response on 17/01/2024."""
+
     class Annotation(BaseModel):
+        """Describe a scheme for the Annotation class on 17/01/2024."""
+
         label: int
         annotation_set: int
 
@@ -33,9 +43,10 @@ class ExtractResponse20240117(BaseModel):
     output=bentoml.io.JSON(pydantic_model=ExtractResponse20240117),
 )
 async def extract(request: ExtractRequest20240117) -> ExtractResponse20240117:
+    """Send an asynchronous call to the Extraction AI and process the response."""
     project = Project(id_=None)
     project.set_offline()
-    category = Category(project=project)
+    category = project.no_category
     document = Document(
         text=request.text,
         bbox=request.bboxes,
@@ -43,12 +54,39 @@ async def extract(request: ExtractRequest20240117) -> ExtractResponse20240117:
         category=category,
     )
     for page in request.pages:
-        Page(id_=page.number, document=document, number=page.number, original_size=page.original_size)
+        Page(document=document, number=page.number, original_size=page.original_size)
 
     result = await extraction_runner.extract.async_run(document)
-    # return result.annotation_sets()
-    res = []
+
+    annotations_result = []
     for annotation_set in result.annotation_sets():
+        current_annotation_set = {'label_set_id': annotation_set.label_set.id_, 'annotations': []}
         for annotation in annotation_set.annotations():
-            res.append({"label": annotation.label.id, "annotation_set": annotation_set.id})
-    return res
+            current_annotation_set['annotations'].append(
+                {
+                    "id": annotation.id_,
+                    "document": annotation.document.id_,
+                    "offset_string": annotation.offset_string,
+                    "offset_string_original": annotation.offset_string_original,
+                    "translated_string": annotation.translated_string,
+                    "normalized": annotation.normalized,
+                    "label": {
+                        "id": annotation.label.id_,
+                        "name": annotation.label.name,
+                        "has_multiple_top_candidates": annotation.label.has_multiple_top_candidates,
+                        "data_type": annotation.label.data_type,
+                        "threshold": annotation.label.threshold,
+                    },
+                    "confidence": annotation.confidence,
+                    "is_correct": annotation.is_correct,
+                    "revised": annotation.is_revised,
+                    "origin": annotation.orgign,
+                    "created_by": annotation.created_by,
+                    "revised_by": annotation.revised_by,
+                    "span": annotation.spans,
+                    "selection_bbox": annotation.selection_bbox,
+                    "custom_offset_string": annotation.custom_offset_string,
+                }
+            )
+        annotations_result.append(current_annotation_set)
+    return annotations_result
