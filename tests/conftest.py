de80ab474b0f2dd819ec7d94c802ed412ec036b5
Liza Ezhergina <k4www4@gmail.com>
2024-07-31 16:28:50 +0200

    Added: handling read timeout with rerun strategy

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import pytest
 def pytest_runtest_makereport(item, call):
     if call.excinfo is not None:
         exc_type, exc_value, _ = call.excinfo
-        if '502' in str(exc_value):
+        if '502' in str(exc_value) or 'Read timed out' in str(exc_value):
             call.excinfo = None
             call._report_outcome = 'rerun'
             call.rerun = True
