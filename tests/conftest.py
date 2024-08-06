97dbdcd9ee2801ce2274bd4ab46d19176c253ef0
Liza Ezhergina <k4www4@gmail.com>
2024-08-06 16:35:59 +0400

    Removed: unused argument

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,24 +23,3 @@ def pytest_runtest_makereport(item, call):
             report.outcome = 'failed'
         else:
             setattr(report, 'rerun', False)
-
-
-def pytest_addoption(parser):
-    parser.addoption('--n_reruns', action='store', default=5, help='Number of times to rerun failed tests')
-
-
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_protocol(item, nextitem):
-    try:
-        reruns = int(item.config.getoption('--n_reruns'))
-    except TypeError:
-        reruns = 0
-    for i in range(reruns + 1):
-        if i > 0:
-            item.ihook.pytest_runtest_logstart(nodeid=item.nodeid, location=item.location)
-        outcome = yield
-        report = outcome.get_result()
-        if not getattr(report, 'rerun', False):
-            break
-        if i == reruns:
-            report.outcome = 'failed'
