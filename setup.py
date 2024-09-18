44805bf490fcfab943dd5176a5f41336503804d7
Giacomo Magnanini <gikappa@gmail.com>
2024-09-18 08:55:39 +0200

    Added: new module in setup.py

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ setuptools.setup(
         'konfuzio_sdk',
         'konfuzio_sdk.bento',
         'konfuzio_sdk.bento.extraction',
+        'konfuzio_sdk.bento.categorization',
         'konfuzio_sdk.tokenizer',
         'konfuzio_sdk.trainer',
     ],
