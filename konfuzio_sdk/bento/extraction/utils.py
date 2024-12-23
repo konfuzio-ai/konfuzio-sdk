086c8ec74cfe38f36890ca9caae27994c22d7d57
Liza Ezhergina <k4www4@gmail.com>
2024-12-23 17:10:24 +0400

    Fixed: source of line_index

--- konfuzio_sdk/bento/extraction/utils.py
+++ konfuzio_sdk/bento/extraction/utils.py
@@ -1,5 +1,6 @@
 """Utility functions for adapting Konfuzio concepts to be used with Pydantic models."""
 import typing as t
+import logging
 
 from pydantic import BaseModel
 
@@ -70,7 +71,7 @@ def prepare_request(request: BaseModel, project: Project, konfuzio_sdk_version: 
                 'y1': bbox.y1,
                 'page_number': bbox.page_number,
                 'text': bbox.text,
-                'line_index': bbox.line_index,
+                'line_index': bbox.line_index
             }
             # Backwards compatibility with Konfuzio SDK versions < 0.3.
             # In newer versions, the top and bottom values are not needed.
@@ -147,7 +148,6 @@ def process_response(result, schema: BaseModel = ExtractResponse20240117) -> Bas
         return schema(annotation_sets=annotations_result)
     elif schema.__name__ == 'ExtractResponseForLegacyTrainer20240912':
         import json
-
         class JSONEncoder(json.JSONEncoder):
             def default(self, obj):
                 if hasattr(obj, 'to_json'):
@@ -217,7 +217,7 @@ def convert_document_to_request(document: Document, schema: BaseModel = ExtractR
                     'top': v.top,
                     'bottom': v.bottom,
                     'text': document.text[k],
-                    'line_index': v.line_index,
+                    'line_index': document.get_bbox()[k]['line_index']
                 }
                 for k, v in document.bboxes.items()
             },
@@ -278,12 +278,9 @@ def convert_response_to_annotations(
     elif response_schema_class.__name__ == 'ExtractResponseForLegacyTrainer20240912':
         """Restore Pandas Dataframe which has been converted to JSON for sending via API."""
         import json
-
         result = response.json()
-
         def my_convert(res_dict):
             import pandas as pd
-
             new_dict = {}
             for k, v in res_dict.items():
                 if isinstance(v, dict):
